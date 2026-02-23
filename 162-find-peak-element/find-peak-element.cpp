f636bb7a4fcf2f7fadaaea2651c9efdbde4e3bab
Chaitanaya25 <chaitanyasrivastava63@gmail.com>
2026-02-23 10:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (30.28%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -7,14 +7,13 @@ public:
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
-            } else {
-                
+            if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
 
-        return left; 
+        return left;
     }
 };
