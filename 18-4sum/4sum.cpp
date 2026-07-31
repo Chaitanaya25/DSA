8de8fb12b3c9ead22249fa1d2266e64df227644b
Chaitanaya25 <chaitanyasrivastava63@gmail.com>
2026-07-31 23:05:14 +0530

    Time: 29 ms (14.03%) | Memory: 17.6 MB (23.83%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
+        vector<vector<int>> ans;
         int n = nums.size();
-        if (n < 4) return res;
-        
+
         sort(nums.begin(), nums.end());
-        
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                
-                int l = j + 1, r = n - 1;
-                
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int l = j + 1;
+                int r = n - 1;
+
                 while (l < r) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[l] + nums[r];
-                    
+                    long long sum = 1LL * nums[i] + nums[j] + nums[l] + nums[r];
+
                     if (sum == target) {
-                        res.push_back({nums[i], nums[j], nums[l], nums[r]});
-                        while (l < r && nums[l] == nums[l + 1]) l++;
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        l++; r--;
-                    } else if (sum < target) {
+                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
                         l++;
-                    } else {
+                        r--;
+
+                        while (l < r && nums[l] == nums[l - 1])
+                            l++;
+
+                        while (l < r && nums[r] == nums[r + 1])
+                            r--;
+                    }
+                    else if (sum < target) {
+                        l++;
+                    }
+                    else {
                         r--;
                     }
                 }
             }
         }
-        
-        return res;
+
+        return ans;
     }
 };
